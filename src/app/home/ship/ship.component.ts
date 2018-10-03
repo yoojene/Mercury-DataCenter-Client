d5d6b4ef4173f2f82795b2ced433edd3884d3c7e
greatfrank <lp_frank@163.com>
2018-10-03 22:43:47 +0800

    fixed #47 Add Pie chart for Ship page

--- src/app/home/ship/ship.component.ts
+++ src/app/home/ship/ship.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from "../../service/http.service";
+import { PostResponse } from "../../module/PostResponse";
+import { Chart } from 'angular-highcharts';
+import { Ship } from "../../module/Ship";
 
 @Component({
   selector: 'app-ship',
@@ -7,9 +11,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipComponent implements OnInit {
 
-  constructor() { }
+  shipsData: any
+  providerChart: Chart
+  providerTable: any
+
+  constructor(
+    private httpService: HttpService
+  ) { }
 
   ngOnInit() {
+    this.httpService.fetchAllByTableName('ships').subscribe(
+      (response: PostResponse) => {
+        this.shipsData = response.msg
+        this.statisticsProvider()
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  statisticsProvider() {
+    let data = []
+    let tempProviderArr = []
+    this.shipsData.forEach((element: Ship) => {
+      tempProviderArr.push(element.provider)
+    });
+    let providerArr = Array.from(new Set(tempProviderArr))
+    providerArr.forEach(element => {
+      let obj = {
+        name: element,
+        y: this.shipsData.filter(function (value) {
+          return value.provider == element
+        }).length
+      }
+      data.push(obj)
+    });
+
+    this.providerTable = data
+    let providerOptions = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'The Provider of Hotel, 2018'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+          }
+        }
+      },
+      series: [{
+        name: 'Pie',
+        colorByPoint: true,
+        data: data
+      }]
+    }
+
+    this.providerChart = new Chart(providerOptions)
+
   }
 
 }
---
+++ src/app/module/Ship.ts
@@ -0,0 +1,8 @@
+export interface Ship {
+  provider: String;
+  type: String;
+  departureDate: String;
+  departurePort: String;
+  journeyDesc: String;
+  journeyType: String;
+}
